5230d9c08539b1f76867741eee4232d02310c1a5
lior <diler.lior@gmail.com>
2021-02-09 18:13:23 +0100

    gamemanager + init scene + base ui + timer

---
+++ Assets/Scripts/GameData.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameData : MonoBehaviour
+{
+
+    private int _highScore = 0;
+    private int _score = 0;
+    private float _timer = 20.00f;
+
+    private void Start()
+    {
+        GameManager.Instance.GameData = this;
+    }
+
+    public int HighScore
+    {
+        get => _highScore;
+        set => _highScore = value;
+    }
+
+    public int Score
+    {
+        get => _score;
+        set => _score = value;
+    }
+
+    public float Timer
+    {
+        get => _timer;
+        set => _timer = value;
+    }
+
+    private void Update()
+    {
+        _timer -= Time.deltaTime;
+    }
+}
---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class UIManager : MonoBehaviour
+{
+    public TextMeshProUGUI scoreTxt;
+    public TextMeshProUGUI highScoreTxt;
+    public TextMeshProUGUI timerTxt;
+    private void Start()
+    {
+        GameManager.Instance.UiManager = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timerTxt.SetText(GameManager.Instance.GameData.Timer.ToString());
+    }
+}
