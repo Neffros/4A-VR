c03b875dc1963d11f219ba09965e53e3c69ecaca
lior <diler.lior@gmail.com>
2021-02-10 20:28:58 +0100

    onselectevent tested

--- Assets/Scripts/testEvent.cs
+++ Assets/Scripts/testEvent.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class testEvent : MonoBehaviour
 {
+
+    public void OnsSelectEvent()
+    {
+        Debug.Log("Selected " + gameObject.name);
+    }
     public void OnActivateEvent()
     {
         Debug.Log("teleporter " + gameObject.name + " used");
