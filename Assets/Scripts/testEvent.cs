ea9713fb06bb2279e470e05d754d7b29bd8e20b6
Lior <diler.lior@gmail.com>
2021-02-10 20:23:40 +0100

    test teleport events

---
+++ Assets/Scripts/testEvent.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testEvent : MonoBehaviour
+{
+    public void OnActivateEvent()
+    {
+        Debug.Log("teleporter " + gameObject.name + " used");
+    }
+}
