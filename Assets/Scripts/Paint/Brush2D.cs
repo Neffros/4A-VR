242b28cfa2a7adacf8b6e5b276787cf4a798b962
Lior <diler.lior@gmail.com>
2021-04-11 18:58:23 +0200

    Base paint structure

---
+++ Assets/Scripts/Paint/Brush2D.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Brush2D : Tool
+{
+    public override void ApplyEffect(ToolTarget target)
+    {
+        throw new System.NotImplementedException();
+    }
+}
---
+++ Assets/Scripts/Paint/Brush3D.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Brush3D : Tool
+{
+    public override void ApplyEffect(ToolTarget target)
+    {
+        throw new System.NotImplementedException();
+    }
+}
