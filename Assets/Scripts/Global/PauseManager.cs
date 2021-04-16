65c57e72103f373f24cf1eee0325794118810a99
JoelNamaZuzzi <59820667+JoelNamaZuzzi@users.noreply.github.com>
2021-04-16 20:43:25 +0200

    1st part for the pause menu

---
+++ Assets/Scripts/Global/PauseManager.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class PauseManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    private XRController xrController;
+
+    public GameObject mainMenuPanel;
+    public GameObject settingsPanel;
+    public Dropdown mainHand;
+    public Dropdown playMode;
+
+
+    public ControllerDict leftHandDict;
+    public ControllerDict rightHandDict;
+    public ControllerDict leftSwordDict;
+    public ControllerDict rightSwordDict;
+
+    public void ReturnLobby()
+    {
+        SceneManager.LoadScene("LevelLobby");
+    }
+
+    public void Continue()
+    {
+        mainMenuPanel.gameObject.SetActive(false);
+        Time.timeScale = 1.0f;
+    }
+
+    public void ExitSettings()
+    {
+        GameManager gameManager = GameManager.Instance;
+        gameManager.SoundManager.Play("selectOption");
+
+        if (mainHand.value == 0)
+            gameManager.GameData.LeftHand = false;
+        else
+            gameManager.GameData.LeftHand = true;
+
+        if (playMode.value == 0)
+            gameManager.GameData.Seated = false;
+        else
+            gameManager.GameData.Seated = true;
+
+        settingsPanel.gameObject.SetActive(false);
+        mainMenuPanel.gameObject.SetActive(true);
+    }
+
+
+    public void DisplaySettings()
+    {
+        GameManager.Instance.SoundManager.Play("selectOption");
+        settingsPanel.gameObject.SetActive(true);
+        mainMenuPanel.gameObject.SetActive(false);
+    }
+}
+
---
+++ Assets/Scripts/Global/PauseMenu.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+
+    private InputDevice leftController;
+    public ControllerDict brushDict;
+    private Transform controllerTransform;
+
+    private GameData gameData;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameData = GameManager.Instance.GameData;
+        gameData.controllerManager.ChangeController(brushDict);
+        leftController = gameData.controllerManager.Controllers[0];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*leftController.TryGetFeatureValue(CommonUsages.trigger, out float triggerValue);
+        if (triggerValue > 0.1f)
+        {
+            
+        }*/
+    }
+}
