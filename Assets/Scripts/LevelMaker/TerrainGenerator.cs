1e671e646c9b2c1ebdd19a13e3551bd3990a4695
Lior <diler.lior@gmail.com>
2021-07-11 03:30:26 +0200

    functionnal chunking rendering based on player pos

--- Assets/Scripts/LevelMaker/TerrainGenerator.cs
+++ Assets/Scripts/LevelMaker/TerrainGenerator.cs
@@ -20,11 +20,14 @@ public enum ChunkSize
 };
 public class TerrainGenerator : MonoBehaviour
 {
+    public GameObject player;
+    public float playerChunkRadius = 50f;
     public Texture2D hMap;
     public ChunkSize ChunkSize;
     public int heightScale = 100;
     public Material terrainMat;
     public Gradient gradient;
+    
     private Texture2D colorText;
     private Color[] pixels;
     private float minTerrainHeight;
@@ -43,62 +46,9 @@ public class TerrainGenerator : MonoBehaviour
         pixels = hMap.GetPixels();
         chunkSize = (int) ChunkSize;
         GenerateAllChunks();
-        //GenerateWholeTerrain();
+
     }
-    /*
-    private void GenerateWholeTerrain()
-    { 
-        mesh = new Mesh();
 
-        Color[] pixels = hMap.GetPixels();
-        
-        colorText = new Texture2D(hMap.width, hMap.height);
-        for (int x = 0; x < hMap.width; x++)
-        {
-            for (int y = 0; y < hMap.height; y++)
-            {
-                float height = pixels[y * hMap.width + x].grayscale * heightScale;
-                Vector3 currVert = new Vector3(x, height, y);
-                verts.Add(currVert);
-                colorText.SetPixel(x, y, Color.red);
-                if (x == 0 || y == 0) continue;
-                    
-                indices.Add(hMap.width * y + x);
-                indices.Add(hMap.width * y + x - 1);
-                indices.Add(hMap.width * (y - 1) + x - 1);
-                indices.Add(hMap.width * (y - 1) + x - 1);
-                indices.Add(hMap.width * (y - 1) + x);
-                indices.Add(hMap.width * y + x);
-
-                if (height > maxTerrainHeight)
-                    maxTerrainHeight = height;
-                if (height < minTerrainHeight)
-                    minTerrainHeight = height;
-            }
-        }
-        //Vector2[] uv = new Vector2[verts.Count];
-        colors = new Color[verts.Count];
-        for (int i = 0; i < verts.Count; i++)
-        {
-            float height = Mathf.InverseLerp(minTerrainHeight, maxTerrainHeight, verts[i].y);
-            colors[i] = gradient.Evaluate(height);
-            //uv[i] = new Vector2(verts[i].x, verts[i].z);
-        }
-
-        GameObject plane = new GameObject("plane from heightmap");
-        plane.AddComponent<MeshFilter>();
-        plane.AddComponent<MeshRenderer>();
-        mesh = new Mesh();
-        mesh.indexFormat = IndexFormat.UInt32;
-        UpdateMesh();
-        
-        plane.GetComponent<MeshRenderer>().material = terrainMat;
-        plane.GetComponent<MeshFilter>().mesh = mesh;
-        plane.AddComponent<TeleportationArea>();
-        plane.transform.position = new Vector3(0, -100, 0);
-        //isGenerated = true;
-    }
-    */
     private void GenerateAllChunks()
     {
         int chunkAmount = hMap.width / chunkSize;
@@ -117,7 +67,6 @@ public class TerrainGenerator : MonoBehaviour
         List<Vector3> verts = new List<Vector3>();
         List<int> indices = new List<int>();
         
-        //Color[] pixels = hMap.GetPixels(startPoint, startPoint, chunkSize, chunkSize);
         Debug.Log(startPointX+chunkSize);
         colorText = new Texture2D(chunkSize, chunkSize);
         for (int x = startPointX; x < startPointX+chunkSize; x++)
@@ -169,23 +118,19 @@ public class TerrainGenerator : MonoBehaviour
         
         plane.GetComponent<MeshRenderer>().material = terrainMat;
         plane.GetComponent<MeshFilter>().mesh = mesh;
+        plane.AddComponent<MeshCollider>();
+        plane.GetComponent<MeshCollider>().convex = true;
+        
         plane.AddComponent<TeleportationArea>();
         plane.transform.position = new Vector3(startPointX, -100, startPointY);
     }
-    /*
-    private void UpdateMesh()
-    {
-    
-        Debug.Log("verts:" + verts.Count);
-        Debug.Log("indices:" + indices.Count);
-        Debug.Log("max indices:" + indices.Max());
-        mesh.Clear();
-        mesh.SetVertices(verts);
-        mesh.SetIndices(indices.ToArray(), MeshTopology.Triangles, 0);
-        mesh.colors = colors;
-        //finalMesh.uv = uv;
-        mesh.RecalculateNormals();
 
+    private void Update()
+    {
+        foreach (var chunk in chunks)
+        {
+            Vector3 chunkPos = chunk.transform.position;
+            chunk.SetActive(Vector3.Distance(chunkPos, player.transform.position) < playerChunkRadius);
+        }
     }
-    */
 }
